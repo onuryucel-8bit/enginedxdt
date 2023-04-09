f848f75d6db7fc817bd8653b1218e81f52e6e0e1
OnurYucel <39805114+onuryucel-8bit@users.noreply.github.com>
2023-04-09 09:01:30 +0300

    vektor carpisma fonksiyonlari eklendi

--- Core/newton/Collision.lua
+++ Core/newton/Collision.lua
@@ -1,7 +1,10 @@
+local Collisions = {}
 
-local function collisionAABB()
-    local A
-    local B
+---comment
+---@param A table
+---@param B table
+---@return boolean
+function Collisions.aabb(A,B)
     
     if 
         A.x < B.x + B.width   and
@@ -13,3 +16,37 @@ local function collisionAABB()
     end
     return false
 end
+
+---vektor kullanan tablolar icin carpisma algilayici
+---@param A table
+---@param B table
+---@return boolean
+function Collisions.VectorAll_xy_aabb(A,B)
+    if 
+        A.location.x            < B.location.x + B.width   and
+        A.location.x + A.width  > B.location.x   and
+        A.location.y            < B.location.y + B.height  and
+        A.location.y + A.height > B.location.y
+    then
+      return true
+    end
+    return false
+end
+
+---A table ,B vector table
+---@param A table
+---@param B table
+---@return boolean
+function Collisions.Vector_xy_aabb(A,B)
+    if 
+        A.x < B.location.x + B.width   and
+        A.x + A.width > B.location.x   and
+        A.y < B.location.y + B.height  and
+        A.y + A.height > B.location.y
+    then
+      return true
+    end
+    return false
+end
+
+return Collisions
