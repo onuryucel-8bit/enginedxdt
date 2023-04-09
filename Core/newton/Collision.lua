204b526623a237b4fa0c0738a77f2c9ba26cc7aa
OnurYucel <39805114+onuryucel-8bit@users.noreply.github.com>
2023-04-09 09:04:03 +0300

    collision,vec2 reloaded

--- Core/newton/Collision.lua
+++ Core/newton/Collision.lua
@@ -1,10 +1,7 @@
-local Collisions = {}
 
----comment
----@param A table
----@param B table
----@return boolean
-function Collisions.aabb(A,B)
+local function collisionAABB()
+    local A
+    local B
     
     if 
         A.x < B.x + B.width   and
@@ -16,37 +13,3 @@ function Collisions.aabb(A,B)
     end
     return false
 end
-
----vektor kullanan tablolar icin carpisma algilayici
----@param A table
----@param B table
----@return boolean
-function Collisions.VectorAll_xy_aabb(A,B)
-    if 
-        A.location.x            < B.location.x + B.width   and
-        A.location.x + A.width  > B.location.x   and
-        A.location.y            < B.location.y + B.height  and
-        A.location.y + A.height > B.location.y
-    then
-      return true
-    end
-    return false
-end
-
----A table ,B vector table
----@param A table
----@param B table
----@return boolean
-function Collisions.Vector_xy_aabb(A,B)
-    if 
-        A.x < B.location.x + B.width   and
-        A.x + A.width > B.location.x   and
-        A.y < B.location.y + B.height  and
-        A.y + A.height > B.location.y
-    then
-      return true
-    end
-    return false
-end
-
-return Collisions
