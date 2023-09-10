f1b72427123468fc53e9204f4df4b8eeebbf0696
onur yucel <onuryucel200@hotmail.com>
2023-09-10 14:10:17 +0300

    simple player movement template

--- Inputs/keyboardInput.lua
+++ Inputs/keyboardInput.lua
@@ -1,43 +1,35 @@
 local keyboardInput={}
 
 keyboardInput.__index = keyboardInput
-function keyboardInput.newKeyboard(stateManager_)
+function keyboardInput.newKeyboard(keys)
 
   local this = {
-   stateManager = stateManager_,
-
-   keyGameState = nil,
-   keyMenuState = nil,
-
+    keys = keys
   }
   setmetatable(this,keyboardInput)
   return this
 end
 
-function keyboardInput:init()
-  local keyGameState_Class = require("Enginedxdt.Inputs.StatesKeyInput.keyGameState")
-  self.keyGameState = keyGameState_Class.newKeyboardInp_GameState(self.stateManager.gameStatePTR)
-
-  local keyMenuState_Class = require("Enginedxdt.Inputs.StatesKeyInput.keyMenuState")
-  self.keyMenuState = keyMenuState_Class.newKeyboardInp_MenuState(self.stateManager)
-end
-
 function keyboardInput:keypressed(key)
+  
   if key == "escape" then
     love.event.quit()
   end
 
-  --oyun durumu icin--
-  if self.stateManager.currentState  == self.stateManager.gameStatePTR then
-    self.keyGameState:keyPressed(key)
-  end   
+  if key == "w"then
+    self.keys.up = true
+  elseif key == "s"then
+    self.keys.down = true
+  end
+  
 end
 
 function keyboardInput:keyreleased(key)
-  --oyun durumu icin--
-  if self.stateManager.currentState  == self.stateManager.gameStatePTR then
-    self.keyGameState:keyReleased(key)
-  end     
+  if key == "w"then
+    self.keys.up = false
+  elseif key == "s"then
+    self.keys.down = false
+  end
 end
 
 return keyboardInput
--- main.lua
+++ main.lua
@@ -1,25 +1,36 @@
 _G.love = love
 
-local function init()
-    
-end
+--VERSION 1.1.18
+
+local player
+local Keyboard
+local keys = {up = false,down = false,left = false, right = false}
 
 function love.keypressed(key)
-   
+   Keyboard:keypressed(key)
 end
 
 function love.keyreleased(key)
-    
+    Keyboard:keyreleased(key)
 end
 
 function love.load()
-    
+    local ckeyboard = require("Inputs.keyboardInput")
+    Keyboard = ckeyboard.newKeyboard(keys)
+
+    local Player = require("entitys.Player")
+    player = Player.new(100,100,32,32)
 end
 
 function love.update(dt)
-    
+    if keys.up then
+       player:update(dt,1) 
+    elseif keys.down then
+       player:update(dt,3) 
+    end
 end
 
 function love.draw()
-
+    
+    player:draw()
 end
